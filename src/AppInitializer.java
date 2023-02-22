cacebe39f7ac61ea824368b07a02a9d65ba5c245
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-02-23 01:12:23 +0530

    Update contened

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -1,5 +1,22 @@
+import lk.ijse.hibernate.entity.Customer;
+import lk.ijse.hibernate.util.FactoryConfiguration;
+import org.hibernate.Session;
+
 public class AppInitializer {
     public static void main(String[] args) {
+        Customer c1 = new Customer();
+
+        c1.setId("C001");
+        c1.setName("Sunimal");
+        c1.setAddress("Galle");
+        c1.setSalary(45451);
+
+
+        Session session = FactoryConfiguration.getInstance().getSession();
+
+        session.save(c1);
+
+        session.close();
 
     }
 }
