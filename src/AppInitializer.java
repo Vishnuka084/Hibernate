55ea2fc73c358010f36201a50ed694f5e4c1598a
Vishnuka yahan <vishnukayahan84@gmail.com>
2023-02-25 11:59:47 +0530

    Select query

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -6,16 +6,18 @@ import org.hibernate.Transaction;
 public class AppInitializer {
     public static void main(String[] args) {
         Customer c2 = new Customer();
-        c2.setId("C002");
+/*        c2.setId("C002");
         c2.setName("sumudu");
         c2.setAddress("jaffna");
-        c2.setSalary(50000);
+        c2.setSalary(50000);*/
 
         Session session = FactoryConfiguration.getInstance().getSession();
         Transaction transaction = session.beginTransaction();
 
-        //for update
-        session.update(c2);
+        //for getting one customer
+        Customer c1 = session.get(Customer.class,"C001");
+        System.out.println(c1.getAddress());
+
 
         transaction.commit();
 
